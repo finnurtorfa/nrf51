9e73b15594a4cc1ff9c22627df6c8563466bf783
Finnur Torfason <finnurtorfa@gmail.com>
2015-04-22 14:46:34 +0200

    Update the startup code

--- lib/nrf51sdk/Nordic/nrf51822/Source/templates/gcc/gcc_startup_nrf51.s
+++ lib/nrf51sdk/Nordic/nrf51822/Source/templates/gcc/gcc_startup_nrf51.s
@@ -1,222 +1,212 @@
-/* 
-Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
-
-The information contained herein is confidential property of Nordic
-Semiconductor ASA.Terms and conditions of usage are described in detail
-in NORDIC SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
-
-Licensees are granted free, non-transferable use of the information. NO
-WARRANTY of ANY KIND is provided. This heading must NOT be removed from
-the file.
+/* File: startup_ARMCM0.S
+ * Purpose: startup file for Cortex-M0 devices. Should use with
+ *   GCC for ARM Embedded Processors
+ * Version: V1.2
+ * Date: 15 Nov 2011
+ *
+ * Copyright (c) 2011, ARM Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the ARM Limited nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ARM LIMITED BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+    .syntax unified
+    .arch armv6-m
 
-/*
-Description message
- */
+    .section .stack
+    .align 3
+#ifdef __STACK_SIZE
+    .equ    Stack_Size, __STACK_SIZE
+#else
+    .equ    Stack_Size, 0xc00
+#endif
+    .globl    __StackTop
+    .globl    __StackLimit
+__StackLimit:
+    .space    Stack_Size
+    .size __StackLimit, . - __StackLimit
+__StackTop:
+    .size __StackTop, . - __StackTop
 
-/* <h> Stack Configuration
-;   <o> Stack Size (in Bytes) <0x0-0xFFFFFFFF:8>
-; </h>
-*/
-  .equ    Stack_Size, 2048
-    .section ".stack", "w"
-    .align  3
-    .globl  __cs3_stack_mem
-    .globl  __cs3_stack_size
-__cs3_stack_mem:
-    .if     Stack_Size
-    .space  Stack_Size
-    .endif
-    .size   __cs3_stack_mem,  . - __cs3_stack_mem
-    .set    __cs3_stack_size, . - __cs3_stack_mem
+    .section .heap
+    .align 3
+#ifdef __HEAP_SIZE
+    .equ    Heap_Size, __HEAP_SIZE
+#else
+    .equ    Heap_Size, 0x100
+#endif
+    .globl    __HeapBase
+    .globl    __HeapLimit
+__HeapBase:
+    .space    Heap_Size
+    .size __HeapBase, . - __HeapBase
+__HeapLimit:
+    .size __HeapLimit, . - __HeapLimit
 
+    .section .isr_vector
+    .align 2
+    .globl __Vectors
+__Vectors:
+    .long    __StackTop            /* Top of Stack */
+    .long    Reset_Handler         /* Reset Handler */
+    .long    NMI_Handler           /* NMI Handler */
+    .long    HardFault_Handler     /* Hard Fault Handler */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    SVC_Handler           /* SVCall Handler */
+    .long    0                     /* Reserved */
+    .long    0                     /* Reserved */
+    .long    PendSV_Handler        /* PendSV Handler */
+    .long    SysTick_Handler       /* SysTick Handler */
 
+    /* External interrupts */
+    .long   POWER_CLOCK_IRQHandler /* POWER_CLOCK */
+    .long   RADIO_IRQHandler       /* RADIO */
+    .long   UART0_IRQHandler       /* UART0 */
+    .long   SPI0_TWI0_IRQHandler   /* SPI0_TWI0 */
+    .long   SPI1_TWI1_IRQHandler   /* SPI1_TWI1 */
+    .long   0                      /* Reserved */
+    .long   GPIOTE_IRQHandler      /* GPIOTE */
+    .long   ADC_IRQHandler         /* ADC */
+    .long   TIMER0_IRQHandler      /* TIMER0 */
+    .long   TIMER1_IRQHandler      /* TIMER1 */
+    .long   TIMER2_IRQHandler      /* TIMER2 */
+    .long   RTC0_IRQHandler        /* RTC0 */
+    .long   TEMP_IRQHandler        /* TEMP */
+    .long   RNG_IRQHandler         /* RNG */
+    .long   ECB_IRQHandler         /* ECB */
+    .long   CCM_AAR_IRQHandler     /* CCM_AAR */
+    .long   WDT_IRQHandler         /* WDT */
+    .long   RTC1_IRQHandler        /* RTC1 */
+    .long   QDEC_IRQHandler        /* QDEC */
+    .long   0                      /* Reserved */
+    .long   SWI0_IRQHandler        /* SWI0 */
+    .long   SWI1_IRQHandler        /* SWI1 */
+    .long   SWI2_IRQHandler        /* SWI2 */
+    .long   SWI3_IRQHandler        /* SWI3 */
+    .long   SWI4_IRQHandler        /* SWI4 */
+    .long   SWI5_IRQHandler        /* SWI5 */
+    .long   0                      /* Reserved */
+    .long   0                      /* Reserved */
+    .long   0                      /* Reserved */
+    .long   0                      /* Reserved */
+    .long   0                      /* Reserved */
+    .long   0                      /* Reserved */
 
-/* <h> Heap Configuration
-;   <o>  Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
-; </h>
-*/
+    .size    __Vectors, . - __Vectors
 
-    .equ    Heap_Size,  2048
-
-    .section ".heap", "w"
-    .align  3
-    .globl  __cs3_heap_start
-    .globl  __cs3_heap_end
-__cs3_heap_start:
-    .if     Heap_Size
-    .space  Heap_Size
-    .endif
-__cs3_heap_end:
-
-/* Vector Table */
-
-    .section ".cs3.interrupt_vector"
-    .globl  __cs3_interrupt_vector_cortex_m
-    .type   __cs3_interrupt_vector_cortex_m, %object
-
-__cs3_interrupt_vector_cortex_m:
-    .long   __cs3_stack                 /* Top of Stack */
-    .long   __cs3_reset                 /* Reset Handler */
-    .long   NMI_Handler                 /* NMI Handler */
-    .long   HardFault_Handler           /* Hard Fault Handler */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   SVC_Handler                 /* SVCall Handler */
-    .long   0                           /* Reserved */
-    .long   0                           /* Reserved */
-    .long   PendSV_Handler              /* PendSV Handler */
-    .long   SysTick_Handler             /* SysTick Handler */
-
-  /* External Interrupts */
-    .long 	POWER_CLOCK_IRQHandler		 /*POWER_CLOCK */
-    .long 	RADIO_IRQHandler		 /*RADIO */
-    .long 	UART0_IRQHandler		 /*UART0 */
-    .long 	SPI0_TWI0_IRQHandler		 /*SPI0_TWI0 */
-    .long 	SPI1_TWI1_IRQHandler		 /*SPI1_TWI1 */
-    .long 	0		 /*Reserved */
-    .long 	GPIOTE_IRQHandler		 /*GPIOTE */
-    .long 	ADC_IRQHandler		 /*ADC */
-    .long 	TIMER0_IRQHandler		 /*TIMER0 */
-    .long 	TIMER1_IRQHandler		 /*TIMER1 */
-    .long 	TIMER2_IRQHandler		 /*TIMER2 */
-    .long 	RTC0_IRQHandler		 /*RTC0 */
-    .long 	TEMP_IRQHandler		 /*TEMP */
-    .long 	RNG_IRQHandler		 /*RNG */
-    .long 	ECB_IRQHandler		 /*ECB */
-    .long 	CCM_AAR_IRQHandler		 /*CCM_AAR */
-    .long 	WDT_IRQHandler		 /*WDT */
-    .long 	RTC1_IRQHandler		 /*RTC1 */
-    .long 	QDEC_IRQHandler		 /*QDEC */
-    .long 	WUCOMP_COMP_IRQHandler		 /*WUCOMP_COMP */
-    .long 	SWI0_IRQHandler		 /*SWI0 */
-    .long 	SWI1_IRQHandler		 /*SWI1 */
-    .long 	SWI2_IRQHandler		 /*SWI2 */
-    .long 	SWI3_IRQHandler		 /*SWI3 */
-    .long 	SWI4_IRQHandler		 /*SWI4 */
-    .long 	SWI5_IRQHandler		 /*SWI5 */
-    .long 	0		 /*Reserved */
-    .long 	0		 /*Reserved */
-    .long 	0		 /*Reserved */
-    .long 	0		 /*Reserved */
-    .long 	0		 /*Reserved */
-    .long 	0		 /*Reserved */
-
-
-    .size   __cs3_interrupt_vector_cortex_m, . - __cs3_interrupt_vector_cortex_m
-
+    .text
     .thumb
-
-/* Reset Handler */
-
-    .equ    NRF_POWER_RAMON_ADDRESS,            0x40000524
-    .equ    NRF_POWER_RAMON_RAMxON_ONMODE_Msk,  0xF  
-
-    .section .cs3.reset,"x",%progbits
     .thumb_func
-    .globl  __cs3_reset_cortex_m
-    .type   __cs3_reset_cortex_m, %function
-__cs3_reset_cortex_m:
-    .fnstart
-    LDR     R0, =NRF_POWER_RAMON_ADDRESS
-    LDR     R2, [R0]
-    MOV     R1, #NRF_POWER_RAMON_RAMxON_ONMODE_Msk
-    ORR     R2, R1
-    STR     R2, [R0]
-    LDR     R0, =SystemInit
-    BLX     R0
-    LDR     R0, =__cs3_start_asm
-    BX      R0
+    .align 2
+    .globl    Reset_Handler
+    .type    Reset_Handler, %function
+Reset_Handler:
+    .equ   NRF_POWER_RAMON_ADDRESS,            0x40000524
+    .equ   NRF_POWER_RAMON_RAM1ON_ONMODE_Msk,  0x3
+    ldr    r0, =NRF_POWER_RAMON_ADDRESS
+    ldr    r2, [r0]
+    movs   r1, #NRF_POWER_RAMON_RAM1ON_ONMODE_Msk
+    orrs   r2, r1
+    str    r2, [r0]
 
+/*     Loop to copy data from read only memory to RAM. The ranges
+ *      of copy from/to are specified by following symbols evaluated in
+ *      linker script.
+ *      __etext: End of code section, i.e., begin of data sections to copy from.
+ *      __data_start__/__data_end__: RAM address range that data should be
+ *      copied to. Both must be aligned to 4 bytes boundary.  */
+    ldr    r1, =__etext
+    ldr    r2, =__data_start__
+    ldr    r3, =__data_end__
+
+    subs    r3, r2
+    ble    .flash_to_ram_loop_end
+
+    movs    r4, 0
+.flash_to_ram_loop:
+    ldr    r0, [r1,r4]
+    str    r0, [r2,r4]
+    adds    r4, 4
+    cmp    r4, r3
+    blt    .flash_to_ram_loop
+.flash_to_ram_loop_end:
+    ldr    r0, =SystemInit
+    blx    r0
+    ldr    r0, =_start
+    bx     r0
     .pool
-    .cantunwind
-    .fnend
-    .size   __cs3_reset_cortex_m,.-__cs3_reset_cortex_m
+    .size Reset_Handler, . - Reset_Handler
 
-    .section ".text"
-
-
-/* Dummy Exception Handlers (infinite loops which can be modified) */
-
-    .weak   NMI_Handler
-    .type   NMI_Handler, %function
-NMI_Handler:
-    B       .
-    .size   NMI_Handler, . - NMI_Handler
-
-
-    .weak   HardFault_Handler
-    .type   HardFault_Handler, %function
-HardFault_Handler:
-    B       .
-    .size   HardFault_Handler, . - HardFault_Handler
-
-
-    .weak   SVC_Handler
-    .type   SVC_Handler, %function
-SVC_Handler:
-    B       .
-    .size   SVC_Handler, . - SVC_Handler
-
-
-    .weak   PendSV_Handler
-    .type   PendSV_Handler, %function
-PendSV_Handler:
-    B       .
-    .size   PendSV_Handler, . - PendSV_Handler
-
-
-    .weak   SysTick_Handler
-    .type   SysTick_Handler, %function
-SysTick_Handler:
-    B       .
-    .size   SysTick_Handler, . - SysTick_Handler
-
-
-/* IRQ Handlers */
-
-    .globl  Default_Handler
-    .type   Default_Handler, %function
-Default_Handler:
-    B       .
-    .size   Default_Handler, . - Default_Handler
-
-    .macro  IRQ handler
-    .weak   \handler
-    .set    \handler, Default_Handler
+/*    Macro to define default handlers. Default handler
+ *    will be weak symbol and just dead loops. They can be
+ *    overwritten by other handlers */
+    .macro    def_default_handler    handler_name
+    .align 1
+    .thumb_func
+    .weak    \handler_name
+    .type    \handler_name, %function
+\handler_name :
+    b    .
+    .size    \handler_name, . - \handler_name
     .endm
 
-    IRQ  POWER_CLOCK_IRQHandler
-    IRQ  RADIO_IRQHandler
-    IRQ  UART0_IRQHandler
-    IRQ  SPI0_TWI0_IRQHandler
-    IRQ  SPI1_TWI1_IRQHandler
-    IRQ  GPIOTE_IRQHandler
-    IRQ  ADC_IRQHandler
-    IRQ  TIMER0_IRQHandler
-    IRQ  TIMER1_IRQHandler
-    IRQ  TIMER2_IRQHandler
-    IRQ  RTC0_IRQHandler
-    IRQ  TEMP_IRQHandler
-    IRQ  RNG_IRQHandler
-    IRQ  ECB_IRQHandler
-    IRQ  CCM_AAR_IRQHandler
-    IRQ  WDT_IRQHandler
-    IRQ  RTC1_IRQHandler
-    IRQ  QDEC_IRQHandler
-    IRQ  WUCOMP_COMP_IRQHandler
-    IRQ  SWI0_IRQHandler
-    IRQ  SWI1_IRQHandler
-    IRQ  SWI2_IRQHandler
-    IRQ  SWI3_IRQHandler
-    IRQ  SWI4_IRQHandler
-    IRQ  SWI5_IRQHandler
+    def_default_handler    NMI_Handler
+    def_default_handler    HardFault_Handler
+    def_default_handler    SVC_Handler
+    def_default_handler    PendSV_Handler
+    def_default_handler    SysTick_Handler
+    def_default_handler    Default_Handler
+    def_default_handler    POWER_CLOCK_IRQHandler
+    def_default_handler    RADIO_IRQHandler
+    def_default_handler    UART0_IRQHandler
+    def_default_handler    SPI0_TWI0_IRQHandler
+    def_default_handler    SPI1_TWI1_IRQHandler
+    def_default_handler    GPIOTE_IRQHandler
+    def_default_handler    ADC_IRQHandler
+    def_default_handler    TIMER0_IRQHandler
+    def_default_handler    TIMER1_IRQHandler
+    def_default_handler    TIMER2_IRQHandler
+    def_default_handler    RTC0_IRQHandler
+    def_default_handler    TEMP_IRQHandler
+    def_default_handler    RNG_IRQHandler
+    def_default_handler    ECB_IRQHandler
+    def_default_handler    CCM_AAR_IRQHandler
+    def_default_handler    WDT_IRQHandler
+    def_default_handler    RTC1_IRQHandler
+    def_default_handler    QDEC_IRQHandler
+    def_default_handler    SWI0_IRQHandler
+    def_default_handler    SWI1_IRQHandler
+    def_default_handler    SWI2_IRQHandler
+    def_default_handler    SWI3_IRQHandler
+    def_default_handler    SWI4_IRQHandler
+    def_default_handler    SWI5_IRQHandler
 
+    .weak    DEF_IRQHandler
+    .set    DEF_IRQHandler, Default_Handler
 
-  .end
-
+    .end
